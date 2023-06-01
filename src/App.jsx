c08bc4a15f7ae20d18677310329e0b6eef9af141
Jaden Stock <jadenstock@hotmail.com>
2023-06-01 10:04:50 +0200

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import Index from "./routes/Index/Index";
-import ReactGA from "react-ga";
+import React, { useState, useEffect } from "react"
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
+import Index from "./routes/Index/Index"
+import ReactGA from "react-ga"
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Archive from "./routes/Archive/Archive";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Archive from "./routes/Archive/Archive"
 
 function App() {
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    ReactGA.initialize("G-J0XEB94HE2");
-    ReactGA.pageview(window.location.pathname + window.location.search);
+	useEffect(() => {
+		ReactGA.initialize("G-J0XEB94HE2")
+		ReactGA.pageview(window.location.pathname + window.location.search)
 
-    document.getElementById("loader").style.display = "none";
-    setLoading(false);
-  }, []);
+		document.getElementById("loader").style.display = "none"
+		setLoading(false)
+	}, [])
 
-  const errorElement = (
-    <div style={{ padding: "300px 50px", textAlign: "center" }}>
-      Sorry. It seems like there's been an error. I'm probably working on it
-      right now.
-    </div>
-  );
+	const errorElement = (
+		<div style={{ padding: "300px 50px", textAlign: "center" }}>
+			Sorry. It seems like there's been an error. I'm probably working on it
+			right now.
+		</div>
+	)
 
-  const router = createBrowserRouter([
-    {
-      path: "/projects",
-      element: <Archive />,
-      errorElement,
-    },
-    {
-      path: "*",
-      element: <Index />,
-      errorElement,
-    },
-  ]);
+	const router = createBrowserRouter([
+		{
+			path: "/projects",
+			element: <Archive />,
+			errorElement
+		},
+		{
+			path: "*",
+			element: <Index />,
+			errorElement
+		}
+	])
 
-  if (loading) {
-    return <div></div>;
-  } else {
-    return (
-      <div>
-        <Navbar />
-        <RouterProvider router={router} />
-        <Footer />
-      </div>
-    );
-  }
+	if (loading) {
+		return <div></div>
+	} else {
+		return (
+			<div>
+				<Navbar />
+				<RouterProvider router={router} />
+				<Footer />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Analytics } from "@vercel/analytics/react"
 
-import App from "./App";
+import App from "./App"
 
-import "./main.css";
-import "./static.css";
+import "./main.css"
+import "./static.css"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<App />
+		<Analytics />
+	</React.StrictMode>
+)
