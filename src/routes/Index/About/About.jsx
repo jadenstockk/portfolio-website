bdbc988a7ad60bfc3168114d39e6a59abde0c1a9
Jaden Stock <jadenstock@hotmail.com>
2024-08-04 15:20:10 +0200

    2024 update

--- src/routes/Index/About/About.jsx
+++ src/routes/Index/About/About.jsx
@@ -1,60 +1,47 @@
-import React from "react"
+import React from "react";
 
 const About = () => {
-	const scratchLink = "https://scratch.mit.edu/users/Magicmanthedj/"
-	const discordLink = "https://discord.com/"
-	const helpsieLink = "https://github.com/jadenstockk/helpsie"
+  const scratchLink = "https://scratch.mit.edu/users/Magicmanthedj/";
+  const discordLink = "https://discord.com/";
+  const helpsieLink = "https://github.com/jadenstockk/helpsie";
 
-	return (
-		<section id="about" className="fade-up" style={{ animationDelay: "700ms" }}>
-			<div className="wrapper">
-				<h2 className="medium-heading-1">
-					<span className="section-number">01.</span> About Me
-				</h2>
-				<p className="description-1">
-					I started learning to code when I was 10 years old on the educational
-					coding platform,{" "}
-					<a href={scratchLink} target="_blank" rel="noopener noreferrer">
-						Scratch
-					</a>
-					. However, my real coding experience started when I began playing
-					around with{" "}
-					<a href={discordLink} target="_blank" rel="noopener noreferrer">
-						Discord
-					</a>
-					's API with the Discord.js framework. This lead me to create{" "}
-					<a href={helpsieLink} target="_blank" rel="noopener noreferrer">
-						Helpsie
-					</a>
-					, a public Discord bot, for other Discord users to use. Through all of
-					this, I had accidentally taught myself JavaScript, Node.js, MongoDB,
-					and soon enough I had also covered basic HTML, CSS, Express.js and
-					also had skills in how to host my code and use the Linux terminal.
-				</p>
-				<p className="description-1">
-					I am now a somewhat experienced React and Next.js web developer with a
-					passion for UI design as well. I am also learning Java at my school.
-					I'm very keen to continue learning and plan to continue working on
-					projects to improve my skills.
-				</p>
-				<p className="description-1">
-					Taking a step back from programming, I am also very passionate about
-					music. I have been playing the piano for almost 11 years now and I'm
-					so grateful to have had the opportunity to learn how to play. When I
-					was in grade 3, my parents bought me a small CD player with two
-					speakers. Being the little entrepreneur I was, I began DJing at my
-					friends parties which quickly turned out to be very profitable. I
-					began upgrading my equipment and eventually managed to buy myself a
-					laptop. It was with this laptop that I began learning how to produce
-					music and record my friends' vocals. This very fortunate background of
-					mine has led me to be very well-versed in music theory, music
-					production, and audio engineering, and allowed me to be a dedicated
-					music student and sound coordinator for concerts and events at my
-					school.
-				</p>
-			</div>
-		</section>
-	)
-}
+  return (
+    <section id="about" className="fade-up" style={{ animationDelay: "700ms" }}>
+      <div className="wrapper">
+        <h2 className="medium-heading-1">
+          <span className="section-number">01.</span> About Me
+        </h2>
+        <p className="description-1">
+          I am an experienced Next.js web developer with a passion for UI design
+          as well. I also have basic experience working with Java. I'm very keen
+          to continue learning and plan to continue working on projects to
+          improve my skills as I continue on to university next year. I have
+          been programming with Javascript and Typescript for almost 4 years now
+          and have worked on a variety of projects, ranging from simple websites
+          to more complex web applications. Through my experience, I have
+          developed a strong understanding of a variety of technologies and
+          frameworks, including React, Next.js, and Node.js. I am also very
+          familiar with Git and have experience working with databases such as
+          MongoDB and Postgres. I am always looking to learn new things and
+          improve my skills, and I am very excited to continue working on
+          projects and learning new technologies.
+        </p>
+        <p className="description-1">
+          Taking a step back from programming, I am also very passionate about
+          music. I have been playing the piano for almost 12 years now and I'm
+          so grateful to have had the opportunity to learn how to play. In grade
+          4, I was able to buy myself a laptop with money I earned from DJ-ing
+          at my friends' parties. It was with this laptop that I began learning
+          how to produce music and record my friends' vocals. This very
+          fortunate background of mine has led me to be very well-versed in
+          music theory, music production, and audio engineering, and allowed me
+          to be a dedicated music student and sound coordinator for concerts and
+          events at my school. It was also that laptop that led me to discover
+          the world of programming and web development, and I am very grateful.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/routes/Index/Hero/Hero.jsx
+++ src/routes/Index/Hero/Hero.jsx
@@ -1,45 +1,46 @@
-import React from "react"
-import faceImage from "../../../assets/face.png"
+import React from "react";
+import faceImage from "../../../assets/face.png";
 
 const Hero = () => {
-	return (
-		<section id="hero">
-			<div className="wrapper">
-				<h3
-					className="small-heading-1 fade-up"
-					style={{ animationDelay: "200ms" }}
-				>
-					Hi there. My name is
-				</h3>
-				<div className="face-name fade-up" style={{ animationDelay: "300ms" }}>
-					<img src={faceImage} alt="" />
-					<h2 className="big-heading-1">Jaden Stock.</h2>
-				</div>
-				<h2
-					className="big-heading-2 fade-up"
-					style={{ animationDelay: "400ms" }}
-				>
-					I am a student developer.
-				</h2>
-				<p
-					className="description-1 fade-up"
-					style={{ animationDelay: "500ms" }}
-				>
-					I'm currently making my way through 11th grade in high school. I have
-					a passion for software development, design, and music. I'm always
-					learning and see great value in being open to new perspectives and
-					ideas.
-				</p>
-				<a
-					className="btn fade-up"
-					style={{ animationDelay: "600ms" }}
-					href="/#projects"
-				>
-					Check out my latest project
-				</a>
-			</div>
-		</section>
-	)
-}
+  return (
+    <section id="hero">
+      <div className="wrapper">
+        <h3
+          className="small-heading-1 fade-up"
+          style={{ animationDelay: "200ms" }}
+        >
+          Hi there. My name is
+        </h3>
+        <div className="face-name fade-up" style={{ animationDelay: "300ms" }}>
+          <img src={faceImage} alt="" />
+          <h2 className="big-heading-1">Jaden Stock.</h2>
+        </div>
+        <h2
+          className="big-heading-2 fade-up"
+          style={{ animationDelay: "400ms" }}
+        >
+          I am a web developer.
+        </h2>
+        <p
+          className="description-1 fade-up"
+          style={{ animationDelay: "500ms" }}
+        >
+          I'm currently making my way through my final year of high school in
+          South Africa. I have a passion for software development, design, and
+          music. I'm always learning and see great value in being open to new
+          perspectives and ideas, whether it be in programming, music, or life
+          in general.
+        </p>
+        <a
+          className="btn fade-up"
+          style={{ animationDelay: "600ms" }}
+          href="/#projects"
+        >
+          Check out my latest project
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
