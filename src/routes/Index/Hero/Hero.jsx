b0bfc0baca7635b031804dbde154de87826ff8d6
Jaden Stock <jadenstock@hotmail.com>
2025-02-16 15:51:33 +0200

    Slight changes

--- src/routes/Index/Hero/Hero.jsx
+++ src/routes/Index/Hero/Hero.jsx
@@ -9,17 +9,17 @@ const Hero = () => {
           className="small-heading-1 fade-up"
           style={{ animationDelay: "200ms" }}
         >
-          Hi there. My name is
+          Hey there! My name is
         </h3>
         <div className="face-name fade-up" style={{ animationDelay: "300ms" }}>
           <img src={faceImage} alt="" />
-          <h2 className="big-heading-1">Jaden Stock.</h2>
+          <h2 className="big-heading-1">Jaden Stock</h2>
         </div>
         <h2
           className="big-heading-2 fade-up"
           style={{ animationDelay: "400ms" }}
         >
-          I am a web developer.
+          I build things for the web
         </h2>
         <p
           className="description-1 fade-up"
@@ -27,6 +27,11 @@ const Hero = () => {
         >
          I’m a first-year computer science student with a passion for software development and design. I thrive on learning and embraceing new perspectives and ideas. My goal is to continuously grow, combining technical skills with creative expression to solve meaningful problems.
         </p>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+        }}>
         <a
           className="btn fade-up"
           style={{ animationDelay: "600ms" }}
@@ -34,6 +39,10 @@ const Hero = () => {
         >
           Check out my latest project
         </a>
+        <a href="mailto:jaden@jadenstock.com"  style={{ animationDelay: "700ms" }} className="btn fade-up">
+					Email me
+				</a>
+        </div>
       </div>
     </section>
   );
